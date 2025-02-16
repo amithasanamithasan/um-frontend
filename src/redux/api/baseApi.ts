21341c2caa96429246deda2f22d17e3886b27cce
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-02-16 11:45:31 +0600

    Handle expired refresh token (kick out the user)

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import { setUser } from "../features/auth/authSlice";
+import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -16,7 +16,7 @@ const baseQuery = fetchBaseQuery({
 
 // 3 paramiters will make
 const baseQuerWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
+  let result = await baseQuery(args, api, extraOptions);
   // console.log(result);
   if (result?.error?.status == 401) {
     console.log("Sending refresh token ");
@@ -26,14 +26,19 @@ const baseQuerWithRefreshToken = async (args, api, extraOptions) => {
       credentials: "include",
     });
     const data = await res.json();
-    console.log(data);
-    const user = (api.getState() as RootState).auth.user;
-    api.dispatch(
-      setUser({
-        user,
-        token: data.data.accessToken,
-      })
-    );
+    // console.log(data);
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        })
+      );
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
   }
   return result;
 };
