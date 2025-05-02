9c515b636ac37eda671cd810f5dd50544a9e611b
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-05-02 21:32:44 +0600

    Create Student basic form structure

--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,7 +1,7 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button } from "antd";
+import { Button, Col, Divider, Row } from "antd";
 
 const studentDummyData = {
   password: "student123",
@@ -11,14 +11,17 @@ const studentDummyData = {
       middleName: "hasan",
       lastName: "Amit",
     },
+
     gender: "male",
     dateOfBirth: "2001-05-20",
+    bloodGroup: "A+",
+
     email: "Studentnew2@example.com",
     contactNo: "01993790088",
     emergencyContactNo: "987-654-3210",
-    bloodGroup: "A+",
     presentAddress: "123 Elm Street, Citytown",
     permanentAddress: "456 Pine Avenue, Villageville",
+
     guardian: {
       fatherName: "James Doe",
       fatherOccupation: "Engineer",
@@ -33,6 +36,7 @@ const studentDummyData = {
       contactNo: "456-789-1234",
       address: "789 Oak Street, Suburban",
     },
+
     admissionSemester: "675d7706ff48d10777d1be46",
     profileImage: "https://example.com/profiles/john.jpg",
     academicDepartment: "676fabc9dbffb8725d12b804",
@@ -41,17 +45,75 @@ const studentDummyData = {
 
 const CreateStudent = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    // console.log(data);
+    console.log(data);
 
-    const formData = new FormData();
-    formData.append("data", JSON.stringify(data));
-    console.log(Object.fromEntries(formData));
+    // const formData = new FormData();
+    // formData.append("data", JSON.stringify(data));
+    // console.log(Object.fromEntries(formData));
   };
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" label="name" />
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Row>
+      <Col span={24}>
+        <PHForm onSubmit={onSubmit}>
+          <Divider style={{ borderColor: "red", color: "red" }}>
+            Personal Information
+          </Divider>
+
+          <Row gutter={8}>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.firstName" label="First Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.middleName" label="Last Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.lastName" label="Last Name" />
+            </Col>
+
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="gender" label="Gender" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="dateOfBirth" label="Death Of Birth" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="bloodGroup" label="Blood Group" />
+            </Col>
+            <Divider style={{ borderColor: "red", color: "red" }}>
+              Contact Information
+            </Divider>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="email" name="email" label="Enter Your Email" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="contactNo" label="Contact Number" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput
+                type="text"
+                name="emergencyContactNo"
+                label="Emergency Contact No"
+              />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput
+                type="text"
+                name="presentAddress"
+                label="Present Address"
+              />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput
+                type="text"
+                name="permanentAddress"
+                label="Permanent Address"
+              />
+            </Col>
+          </Row>
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Row>
   );
 };
 
