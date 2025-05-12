5c2c2c0e322d472c7b1779d4804ddbe948849ba5
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-05-12 23:12:41 +0600

    Semester Registration details

--- src/pages/admin/courseManagement/RegisteredSemesters.tsx
+++ src/pages/admin/courseManagement/RegisteredSemesters.tsx
@@ -1,10 +1,93 @@
+import { Button, Table, TableColumnsType, TableProps } from "antd";
+
+import { TAcademicSemester } from "../../../types/academicManagement.type";
+import { useGetAllRegisteredSemestersQuery } from "../../../redux/features/admin/courseManagement";
+
+export type TTableData = Pick<
+  TAcademicSemester,
+  "name" | "year" | "startMonth" | "endMonth"
+>;
 const RegisteredSemesters = () => {
+  // const [params, setParams] = useState<TQueryParams[] | undefined>(undefined);
+  // const { data: semesterData } = useGetAllSemesterQuery([
+  //   { name: "name", value: "Summer" },
+  // ]);
+  const {
+    data: semesterData,
+    isLoading,
+    isFetching,
+  } = useGetAllRegisteredSemestersQuery(undefined);
+  const tableData = semesterData?.data?.map(
+    ({ _id, academicSemester, startDate, endDate, status }) => ({
+      key: _id,
+      name: `${academicSemester.name} ${academicSemester.year}`,
+      startDate,
+      endDate,
+      status,
+    })
+  );
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "name",
+    },
+    {
+      title: "Status",
+      key: "status",
+      dataIndex: "status",
+    },
+    {
+      title: "Start Date",
+      key: "startDate",
+      dataIndex: "startDate",
+    },
+    {
+      title: "End Date",
+      key: "endDate",
+      dataIndex: "endDate",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return (
+          <div>
+            <Button>Update</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  // const onChange: TableProps<TTableData>["onChange"] = (
+  //   _pagination,
+  //   filters,
+  //   _sorter,
+  //   extra
+  // ) => {
+  //   // console.log({ pagination, filters, sorter, extra });
+  //   if (extra.action == "filter") {
+  //     const queryParams: TQueryParams[] = [];
+
+  //     // console.log(queryParams);
+  //     setParams(queryParams);
+  //   }
+  // };
+  // if (isLoading) {
+  //   return <p>Loading</p>;
+  // }
+  // if (isFetching) {
+  //   return <div>Refreshing data...</div>;
+  // }
   return (
-    <div>
-      <h1>
-        <p>This is our RegisterSemester Pages </p>
-      </h1>
-    </div>
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      // onChange={onChange}
+    />
   );
 };
 
--- src/redux/features/admin/courseManagement.ts
+++ src/redux/features/admin/courseManagement.ts
@@ -1,30 +1,31 @@
+import { TQueryParams, TRessponseRedux, TSemester } from "../../../types";
 import { baseApi } from "../../api/baseApi";
 
 const courseManagementApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    // getAllSemesters: builder.query({
-    //   query: (args) => {
-    //     const params = new URLSearchParams();
+    getAllRegisteredSemesters: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
 
-    //     if (args) {
-    //       args.forEach((item: TQueryParams) => {
-    //         params.append(item.name, item.value as string);
-    //       });
-    //     }
+        if (args) {
+          args.forEach((item: TQueryParams) => {
+            params.append(item.name, item.value as string);
+          });
+        }
 
-    //     return {
-    //       url: "/academic-semesters",
-    //       method: "GET",
-    //       params: params,
-    //     };
-    //   },
-    //   transformResponse: (response: TRessponseRedux<TAcademicSemester[]>) => {
-    //     return {
-    //       data: response.data,
-    //       meta: response.meta,
-    //     };
-    //   },
-    // }),
+        return {
+          url: "/semester-registrations",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TRessponseRedux<TSemester[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
     addRegisteredSemester: builder.mutation({
       query: (data) => ({
         url: "/semester-registrations/create-semester-registration",
@@ -35,4 +36,7 @@ const courseManagementApi = baseApi.injectEndpoints({
   }),
 });
 
-export const { useAddRegisteredSemesterMutation } = courseManagementApi;
+export const {
+  useAddRegisteredSemesterMutation,
+  useGetAllRegisteredSemestersQuery,
+} = courseManagementApi;
