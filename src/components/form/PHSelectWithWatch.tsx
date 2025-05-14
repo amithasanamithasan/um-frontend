fea7afb5d4d8dd808d73a255dc07c29f665d49db
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-05-14 23:12:59 +0600

    Subscribe to a particular form element

---
+++ src/components/form/PHSelectWithWatch.tsx
@@ -0,0 +1,47 @@
+import { Form, Select } from "antd";
+
+import { Controller, useFormContext, useWatch } from "react-hook-form";
+
+type TPHSelectProps = {
+  label: string;
+  name: string;
+  options: { value: string; label: string; disabled?: boolean }[] | undefined;
+  disabled?: boolean;
+  mode?: "multiple" | undefined;
+  onValueChange: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const PHSelectWithWatch = ({
+  label,
+  name,
+  options,
+  disabled,
+  mode,
+}: TPHSelectProps) => {
+  const method = useFormContext();
+  const inputValue = useWatch({
+    control: method.control,
+    name,
+  });
+  console.log(inputValue);
+  return (
+    <Controller
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <Form.Item label={label}>
+          <Select
+            mode={mode}
+            style={{ width: "100%" }}
+            {...field}
+            options={options}
+            size="large"
+            disabled={disabled}
+          />
+          {error && <small style={{ color: "red" }}>{error.message}</small>}
+        </Form.Item>
+      )}
+    />
+  );
+};
+
+export default PHSelectWithWatch;
