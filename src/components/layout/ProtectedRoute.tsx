5418bb76aa99795c6666e237405044c46a153b4d
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-05-19 12:18:05 +0600

    current user have login show my ui

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -1,10 +1,25 @@
 import { ReactNode } from "react";
 import { useAppSelector } from "../../redux/hooks";
-import { useCurrentToken } from "../../redux/features/auth/authSlice";
+import {
+  selectCurrentUser,
+  useCurrentToken,
+} from "../../redux/features/auth/authSlice";
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+type TProtectedRouteProps = {
+  children: ReactNode;
+  role: string;
+};
+
+const ProtectedRoute = ({ children, role }: TProtectedRouteProps) => {
   const token = useAppSelector(useCurrentToken); // Directly get the token value
+  const user = useAppSelector(selectCurrentUser);
+  console.log(user);
+  if (role === "admin") {
+    if (!token) {
+      return <Navigate to="/login" replace={true} />;
+    }
+  }
 
   if (!token) {
     return <Navigate to="/login" replace={true} />;
